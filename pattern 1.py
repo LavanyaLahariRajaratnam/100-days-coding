3d3244c1bc88c1471bc60a87a663489c37eb8e1e
LavanyaLahariRajaratnam <69295312+LavanyaLahariRajaratnam@users.noreply.github.com>
2020-09-01 11:48:52 +0530

    Add files via upload

---
+++ pattern 1.py
@@ -0,0 +1,18 @@
+'''for i in range(1,6):
+    for j in range(1,6):
+        print(j,end='')
+    print()
+'''
+'''
+n=5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print('*',end='')
+    print()
+'''
+'''
+n=5
+for i in range(1,n+1):
+    print(i*'*')
+'''
+
